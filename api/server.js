af980c32f23715749d8d51378ad3fa97c53299c8
prithvi <21jaisingh@gmail.com>
2020-09-05 22:16:56 +0530

    heroku sending requests to both app and localhost

--- api/server.js
+++ api/server.js
@@ -22,7 +22,7 @@ app.use(cors());
 
 // a middleware with no mount path; gets executed for every request to the app
 app.use(function(req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin',clientURL);
+  res.setHeader('Access-Control-Allow-Origin', clientURL);
   next();
 });
 
