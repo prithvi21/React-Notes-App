d6a03f0d879ad9ec4d9f450f51adb0ec01a94c6e
prithvi <21jaisingh@gmail.com>
2020-09-05 21:17:24 +0530

    changes

---
+++ api/build/precache-manifest.641165f05794823a1b8033676f81f6b3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60e4c510f55c1b5fae3ed025c65af8f9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a24ff4ea9490a3cd72a7",
+    "url": "/static/css/main.14dfc8a9.chunk.css"
+  },
+  {
+    "revision": "86e476a2c46dc1c95d97",
+    "url": "/static/js/2.c2a61a60.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c2a61a60.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a24ff4ea9490a3cd72a7",
+    "url": "/static/js/main.194cfd82.chunk.js"
+  },
+  {
+    "revision": "01ff5abfa2aca608c6fc",
+    "url": "/static/js/runtime-main.6ee577dc.js"
+  },
+  {
+    "revision": "934d0dbac44bc834fd6a54f62c28f202",
+    "url": "/static/media/close-img.934d0dba.png"
+  },
+  {
+    "revision": "0a60f83e0730d368b95b029c2d996d9f",
+    "url": "/static/media/user-settings.0a60f83e.png"
+  }
+]);
