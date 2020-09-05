42438ad3dab12ce795679abe1df07dacbf604005
prithvi <21jaisingh@gmail.com>
2020-09-06 01:14:12 +0530

    removed build from gitgnore

---
+++ build/precache-manifest.033ab62309a2efeace6e858509467cb5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e9d5329d8f7da4b41e35be7765d66cc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c38733137e7f3f256360",
+    "url": "/static/css/main.14dfc8a9.chunk.css"
+  },
+  {
+    "revision": "86e476a2c46dc1c95d97",
+    "url": "/static/js/2.c2a61a60.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c2a61a60.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c38733137e7f3f256360",
+    "url": "/static/js/main.6a617d0f.chunk.js"
+  },
+  {
+    "revision": "01ff5abfa2aca608c6fc",
+    "url": "/static/js/runtime-main.6ee577dc.js"
+  },
+  {
+    "revision": "934d0dbac44bc834fd6a54f62c28f202",
+    "url": "/static/media/close-img.934d0dba.png"
+  },
+  {
+    "revision": "0a60f83e0730d368b95b029c2d996d9f",
+    "url": "/static/media/user-settings.0a60f83e.png"
+  }
+]);
