b29a64cb0f6ee547f7c21f9fdcc729e761a8e418
prithvi <21jaisingh@gmail.com>
2020-09-05 18:05:48 +0530

    changes

--- api/server.js
+++ api/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const session = require('express-session');
-const PORT = process.env.PORT || 80;
+const PORT = process.env.PORT || 5000;
 const cors = require('cors');
 const path = require('path');
 const clientURL = "https://reactnote-app.herokuapp.com";
@@ -25,6 +25,7 @@ app.use(cors());
 // });
 
 app.get('/', function(req,res) {
+  console.log('get react app');
   res.sendFile(path.join(__dirname, '../build', 'index.html'));
 });
 
@@ -155,4 +156,4 @@ app.post('/create', function(req,res) {
   });
 });
 
-app.listen(PORT);
+app.listen(PORT,  () => console.log(`server running on port ${PORT}`));
