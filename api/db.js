0cfbf564e29055f9325e97c4703e2e1c270318f8
Prithvi <21jaisingh@gmail.com>
2020-09-16 10:48:59 +0530

    app crashed

--- api/db.js
+++ api/db.js
@@ -180,11 +180,7 @@ function sendNotesToClient(userID) {
 }
 
 // sendNotesToClient(1).then(res => console.log(res)).catch(err => console.log(err));
-async function fun(){
-  let p =await getIDFromUsername('Prithvi').then(r => {return r}).catch();
-  console.log(p);
-}
-fun();
+
 
 
 module.exports = { createUser, validateUser, getIDFromUsername, createUserTable, insertNotes, sendNotesToClient, getAllUsernames };
